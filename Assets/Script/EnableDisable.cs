40148be62016bdc29a8123418b1c3f6349bb1fd4
Ziting Sun <sunziti@shernet.sheridancollege.ca>
2025-01-30 10:48:58 -0500

    add sound effect

--- Assets/Script/EnableDisable.cs
+++ Assets/Script/EnableDisable.cs
@@ -7,7 +7,8 @@ public class EnableDisable : MonoBehaviour
     public GameObject go;
     public SpriteRenderer sr;
     public EnableDisable script;
-
+    public AudioSource audioSouces;
+    public AudioClip clip;
     void Update()
     {
         if (Input.GetMouseButtonDown(0)) 
@@ -22,6 +23,11 @@ public class EnableDisable : MonoBehaviour
             sr.enabled = true;
             //script.enabled = true;
             go.SetActive(true);
+        }
+        if (Input.GetKey(KeyCode.Space)) //if(audioSource.GetKey(KeyCode.Space)--hold the key and kept playing.
+        {
+            //audioSouces.Play();
+            audioSouces.PlayOneShot(clip);
         }
     }
 }
