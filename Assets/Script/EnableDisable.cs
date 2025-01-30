be6e740c0d0d298751ee45a73e138493fd4aba1a
Ziting Sun <sunziti@shernet.sheridancollege.ca>
2025-01-30 10:31:44 -0500

    turning things on and off

---
+++ Assets/Script/EnableDisable.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableDisable : MonoBehaviour
+{
+    public GameObject go;
+    public SpriteRenderer sr;
+    public EnableDisable script;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            sr.enabled = false;
+            //script.enabled = false;
+            go.SetActive(false);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            sr.enabled = true;
+            //script.enabled = true;
+            go.SetActive(true);
+        }
+    }
+}
