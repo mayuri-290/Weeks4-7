b5b7ac0ef99646a4b0084b62e602aab5cda138fb
Ziting Sun <sunziti@shernet.sheridancollege.ca>
2025-02-08 20:33:00 -0500

    added planet rotation code

--- Assets/Explorable Toy/planetRotation.cs
+++ Assets/Explorable Toy/planetRotation.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class planetRotation : MonoBehaviour
 {
+    public float speed = 5f; //set rotation speed
+    public bool mouseDragging = false;//set a boolean expression for mouse click
+    private Vector2 lastMousePosition;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,25 @@ public class planetRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+    if(Input.GetMouseButtonDown(0))
+        {
+            mouseDragging = true;
+            lastMousePosition = Input.mousePosition;
+        }
+    else
+            if(Input.GetMouseButtonUp(0))
+        {
+            mouseDragging = false;
+        }
+
+        if (mouseDragging)
+        {
+            Vector2 currentMousePos = Input.mousePosition;
+           
+            float moveX = currentMousePos.x - lastMousePosition.x;
+            transform.Rotate(0, 0, -moveX * 0.5f);
+            lastMousePosition = currentMousePos;
+        }
+
     }
 }
