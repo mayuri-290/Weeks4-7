6ac4951ad149ee80edd4a968151b97297dd9414c
Ziting Sun <sunziti@shernet.sheridancollege.ca>
2025-02-16 16:53:51 -0500

    Added changes: changed mouse drag to slider rotation

--- Assets/Explorable Toy/planetRotation.cs
+++ Assets/Explorable Toy/planetRotation.cs
@@ -1,42 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class planetRotation : MonoBehaviour
 {
-    public float speed = 5f; //set rotation speed
-    public bool mouseDragging = false;//set a boolean expression for mouse click
-    private Vector2 lastMousePosition;
+    public Slider planetRot;
+    public float PlanetRotScale;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-    if(Input.GetMouseButtonDown(0))
-        {
-            mouseDragging = true;
-            lastMousePosition = Input.mousePosition;
-        }
-    else
-            if(Input.GetMouseButtonUp(0))
-        {
-            mouseDragging = false;
+
+        PlanetRotScale = planetRot.value * 1f;
+        this.transform.rotation = Quaternion.Euler(0, 0, PlanetRotScale);
+
         }
 
-        if (mouseDragging)
-        {
-            Vector2 currentMousePos = Input.mousePosition;
-           
-            float moveX = currentMousePos.x - lastMousePosition.x;
-            transform.Rotate(0, 0, -moveX * 0.5f);
-            lastMousePosition = currentMousePos;
-        }
-
-    }
 }
+
