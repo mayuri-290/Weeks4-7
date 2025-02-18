7e8cc75dd5a9c92cafcb96cd5b5c472ef66d6d9d
DESKTOP-9F7IE3V\mayuri <sunziti@sheridancollege.ca>
2025-02-17 22:21:17 -0500

    add text timing to the timer slide

--- Assets/Explorable Toy/ExplodeTiming.cs
+++ Assets/Explorable Toy/ExplodeTiming.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ExplodeTiming : MonoBehaviour
 {
     public Slider sliderTimer;
     public float t = 2f;
+
+    public TextMeshProUGUI timerText;
+  
 
     // Start is called before the first frame update
     void Start()
@@ -14,11 +18,26 @@ public class ExplodeTiming : MonoBehaviour
         sliderTimer.minValue = 0;
         sliderTimer.maxValue = 5;
         sliderTimer.value = t;
+
+        UpdateTimerText();
     }
 
     // Update is called once per frame
     void Update()
     {
         sliderTimer.value -= Time.deltaTime*0.3f;
+
+        sliderTimer.value = Mathf.Max(sliderTimer.value, 0);
+
+        UpdateTimerText();
+
     }
+    void UpdateTimerText()
+    {
+        if (timerText)
+        {
+            timerText.text = "Time: " + sliderTimer.value.ToString("0.00");
+        }
+    }
+
 }
