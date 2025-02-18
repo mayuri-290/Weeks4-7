8088f7b2f054180d4183772650e7adfd3f39160c
DESKTOP-9F7IE3V\mayuri <sunziti@sheridancollege.ca>
2025-02-17 21:38:46 -0500

    changed the background change codes.

--- Assets/Explorable Toy/backgroundChange.cs
+++ Assets/Explorable Toy/backgroundChange.cs
@@ -1,19 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class backgroundChange : MonoBehaviour
 {
-    public Image backgroundImage;
-    public Sprite bg1;
-    public Sprite bg2;
-    public Sprite bg3;
+    public SpriteRenderer bg1;
+    public SpriteRenderer bg2;
+    public SpriteRenderer bg3;
+
+
+    private int currentBackground=1;
 
     // Start is called before the first frame update
- 
 
-  //  public void ChangeBackground()
+     void Start()
+    {
+       
+    }
+
+    public void ChangeBackground()
+    {
+        currentBackground = (currentBackground % 3) + 1;
+        UpdateBackground();
+    }
+
+    void UpdateBackground()
+    {
+        bg1.gameObject.SetActive(currentBackground == 1);
+        bg2.gameObject.SetActive(currentBackground == 2);
+        bg3.gameObject.SetActive(currentBackground == 3);
+    }
+
+    
 
 
 
