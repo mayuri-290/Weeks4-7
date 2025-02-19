227b494d67f6a49a48ce97dc5ab4f4ec764a3795
Ziting Sun <sunziti@shernet.sheridancollege.ca>
2025-02-19 01:47:03 -0500

    changed name of file

--- Assets/Week 5/playerMovement.cs
+++ Assets/Week 5/playerMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerMove : MonoBehaviour
+public class playerMovement : MonoBehaviour
 {
     public float speed = 5;
     public GameObject carrot;
