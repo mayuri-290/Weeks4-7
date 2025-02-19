204784d1ebe4c4e5ef3e54d33455efad4e017c2b
Ziting Sun <sunziti@shernet.sheridancollege.ca>
2025-02-18 19:56:47 -0500

    added mouse tracking alien comments

--- Assets/Explorable Toy/mousetrackingAlien.cs
+++ Assets/Explorable Toy/mousetrackingAlien.cs
@@ -2,24 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//This script is for an alien character following the mouse when the right mouse button is clicked. 
+
 public class mousetrackingAlien : MonoBehaviour
 {
-    private bool mouseFollow = false;
+    private bool mouseFollow = false;//set a bool statement to check if the alien should follow the mouse.
+
 
     // Update is called once per frame
     void Update()
     {
+        //I add a if statement and use getmousebuttonddown, since the mouse should be clicked only once.
+        //Set the number to 1, because the "right" mouse button will be clicked. 
         if (Input.GetMouseButtonDown(1))
         {
             mouseFollow = true;
 
         }
-
+        //Add one more if statement, when mouse floow is activated, update the alien position.
             if(mouseFollow)
             {
-            Vector3 mousePosition = Input.mousePosition;
+            Vector3 mousePosition = Input.mousePosition; //to get the current mouse position in screen coordinates.
+
             mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, 10));
+            //I use STWP to make sure the alien follow the mouse properly. 
+
             transform.position = new Vector3(mousePosition.x, mousePosition.y, transform.position.z);
+            //update the alien's position to follow the mouse. 
         }
     }
 
