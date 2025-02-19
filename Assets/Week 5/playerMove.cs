048f82e114b29c419f3ea495eeefd3d64d9322d9
Ziting Sun <sunziti@shernet.sheridancollege.ca>
2025-02-19 01:45:47 -0500

    add player move coding gym code

---
+++ Assets/Week 5/playerMove.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+    public float speed = 5;
+    public GameObject carrot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = transform.position;
+        pos.x += Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        pos.y += Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        transform.position = pos;
+
+    }
+}
